0534959a6e752308c61679b49fd599389a1422cf
asmwarrior <asmwarrior@gmail.com>
2025-12-31 10:03:39 +0800

    add the print_ast function

--- cpp_parser_grammar.h
+++ cpp_parser_grammar.h
@@ -2,6 +2,7 @@
 #define CPP_PARSER_GRAMMAR_H
 
 #include <string>
+#include <iostream>
 #include <unordered_map>
 #include "parserlib.hpp"
 
@@ -88,14 +89,14 @@ public:
     // --- parse ---
     bool parse(parse_context_type& pc);
 
-//    template <typename Node>
-//    static void print_ast(const Node& node, int indent = 0) {
-//        std::string space(indent, ' ');
-//        std::cout << space << match_id_to_string(node->id()) << space
-//                  << node->children().size() << " \"" << node->source() << "\"\n";
-//        for (auto& c : node->children())
-//            print_ast(c, indent + 2);
-//    }
+    template <typename Node>
+    static void print_ast(const Node& node, int indent = 0) {
+        std::string space(indent, ' ');
+        std::cout << space << match_id_to_string(node->id()) << space
+                  << node->children().size() << " \"" << node->source() << "\"\n";
+        for (auto& c : node->children())
+            print_ast(c, indent + 2);
+    }
 
 
 private:
